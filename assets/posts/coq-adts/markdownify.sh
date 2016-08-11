7d97d017d173ee6283d073bfa97806c56943eed6
Konstantin Weitz <weitzkon@cs.uw.edu>
2016-08-10 23:02:52 -0700

    made changes

--- assets/posts/coq-adts/markdownify.sh
+++ assets/posts/coq-adts/markdownify.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-echo "<!-- FILE AUTOMATICALLY GENERATED FROM '$1'. -->" > $2
-echo "<!-- DO NOT MODIFY THIS FILE MANUALLY! -->" >> $2
-echo >> $2
+# echo "<!-- FILE AUTOMATICALLY GENERATED FROM '$1'. -->" > $2
+# echo "<!-- DO NOT MODIFY THIS FILE MANUALLY! -->" >> $2
+# echo >> $2
 
-cat $1 | sed 's/(\*\*//g' | sed 's/\*\*)//g' >> $2
+cat $1 | sed 's/(\*\*//g' | sed 's/\*\*)//g' > $2
